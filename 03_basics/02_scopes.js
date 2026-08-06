7f27172a5df3708da2a134270d3adb6fc3053368
shivani10sharma2004-source <shivani10sharma2004@gmail.com>
2026-08-06 08:51:59 +0530

    update Both files

---
+++ 03_basics/02_scopes.js
@@ -0,0 +1,97 @@
+//let a=169
+//const b=200
+//var c =500
+//if(true){
+   // let a=10
+    //const b=20
+    //var c =50
+//}
+
+//console.log(a);
+//console.log(b);
+//console.log(c);
+
+
+
+
+let a=2345
+var c=6543
+const b=8965
+
+if(true){
+    let a=10
+    const b=20
+    let c =50 
+    console.log("INNER: ",a);   //[If we do not use var in block but they give a  different value] 
+}
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+
+//***************************Function Scope***************************//
+
+function greet(){
+    let name="Shivani";
+    console.log(name);
+}
+greet();   //[In function it first go on a outside the function where greet() is called and then go inside a function block]
+
+
+function greet(){
+    let name="Shivani";
+}
+greet();
+//console.log(name);
+
+
+let city="Delhi";
+function show(){
+    let city="punjab";
+    //console.log(city);
+}
+show();
+//console.log(city);  
+
+
+function one(){
+    const username="shivani";
+    function two(){
+        const website="youtube";
+      //  console.log(username);
+    }
+    //console.log(website);
+    two();
+}
+one();
+
+
+if(true){
+    const username="shivani"
+    if(username=="shivani"){    //(== compare only value but ===compare both value and type)
+        const Lastname=" sharma"
+        //console.log(username+Lastname);  //[In this case it check first if block if it is true than check second block is true if it is true then execote secon block bcz of console.log]
+
+    }
+    //console.log(website);
+}
+//console.log(username);
+
+
+//***************************Interesting**************************************/
+
+
+console.log(addOne(5));
+function addOne(num){
+    return num+1;
+}
+
+
+
+console.log(addtwo(4));;
+
+const addtwo=function(num){
+    return num+2;
+}
+addtwo(4);
---
+++ 03_basics/03_This.js
@@ -0,0 +1,73 @@
+const StudentData={
+       Name:"Shivani",
+       Age:21,
+
+       AddressVerify:function(){
+        console.log(`${this.Name}, your address is Incorrect`);
+        //console.log(this);
+       }
+}
+//StudentData.AddressVerify();
+//StudentData.Name="Sujal"
+//StudentData.AddressVerify();
+//console.log(this); // [But in browser there is many global windows object when we run this keyword]
+
+
+function Name(){
+    let marks=67;
+    console.log(this);   //[Provide global values]
+}
+Name();
+
+function Data(){
+    let marks=67;
+    console.log(this.marks); //[The output will be undefined]
+}
+Data();
+
+function AnotherData(){
+    this.marks=22;
+    console.log(this.marks);
+}
+AnotherData();
+
+
+//const myName=function(){
+ //   let MyRollNo=18;
+    //console.log(this.MyRollNo);
+//}
+//myName();
+
+const myName=() =>{        //[This is arrow Function]
+   let MyRollNo=18;
+    console.log(this.MyRollNo);  //[when we run this.MyrollNo the output will be undefined and we only run this the output will be empty object ]
+
+   }
+myName();
+
+//*******************Basic Syntax of arrow function**************************//
+
+()=>{}
+
+//const userid=(num1,num2)=>{
+  //  return num1+num2                    //[It is called explicit return]
+//}
+//console.log(userid(7,55));
+
+
+//const userid=(num1,num2)=> num1+num2       //[It is called Implicit Return]
+//const userid=(num1,num2)=>(num1+num2)    //[if we write in curly braces then we have  to write return statement]
+const userid=(num1,num2)=>({username:'shivani'})//[if we return object we have to return like this]
+
+
+console.log(userid(7,55));
+
+
+
+
+
+
+
+
+
+
